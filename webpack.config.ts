9111bca420c989ac98ea638f33585bbb865d964f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-08 21:19:55 +0200

    Getting started with TypeScript optional use
    The TS has been used to allow configuration type checking.
    See: https://medium.com/webpack/unambiguous-webpack-config-with-typescript-8519def2cac7

---
+++ webpack.config.ts
@@ -0,0 +1,27 @@
+import * as webpack from "webpack";
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PATHS = {
+  app: path.join(__dirname, "app"),
+  build: path.join(__dirname, "build")
+};
+
+const config: webpack.Configuration = {
+  // Entries have to resolve to files! They rely on Node
+  // convention by default so if a directory contains *index.js*,
+  // it resolves to that.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: "[name].js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack demo"
+    })
+  ]
+};
+export default config;

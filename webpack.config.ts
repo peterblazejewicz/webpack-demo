b00f327513497ccab63a0fa10293f5d6f293f1a0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-08 22:41:11 +0200

    Small fixes

--- webpack.config.ts
+++ webpack.config.ts
@@ -36,14 +36,12 @@ const developmentConfig = () => {
       // routing works. Good for complex setups.
       historyApiFallback: true,
       // Parse host and port from env to allow customization.
-      //
       // If you use Docker, Vagrant or Cloud9, set
       // host: options.host || '0.0.0.0';
-      //
       // 0.0.0.0 is available to all network devices
       // unlike default `localhost`.
       host: process.env.HOST, // Defaults to `localhost`
-      port: +(process.env.PORT || 8080), // Defaults to 8080
+      port: +(process.env.PORT || '8080'), // Defaults to 8080
       publicPath: "/",
       // Display only errors to reduce the amount of output.
       stats: "errors-only",
